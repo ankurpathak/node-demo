84fb4bc18aa9755f89687a895a0944fff0a45ab9
Ankur Pathak <adpathak@lentra.ai>
2020-06-22 00:57:48 +0530

    app and add File Added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+/*
+- Every Javascript file is module
+- Modules are encapsulated by default ie gone after execution not accessible to other module by default even after import
+*/
+function greet(name){
+    console.log(`Hey there, ${name}!`);
+}
+
+greet('Ankur');
