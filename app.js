9e94b247d33b0430ff3aac5c6e25eaf8c7ae49b1
Ankur Pathak <adpathak@lentra.ai>
2020-06-22 01:05:36 +0530

    import add at the top in app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 - Every Javascript file is module
 - Modules are encapsulated by default ie gone after execution not accessible to other module by default even after import
 */
+require('./add');
 function greet(name){
     console.log(`Hey there, ${name}!`);
 }
